8522ad65da08a5d089657bc71b72a472d3d98294
Pradhuman Singh <pradhumansingh871@gmail.com>
2023-09-26 00:35:33 +0530

    Updated Rock Paper Scissor.js

--- RockPaperScissor/script.js
+++ RockPaperScissor/script.js
@@ -7,10 +7,32 @@ const values=["img/rock.png","img/paper.png","img/scissors.png"];
 
 function start(){
 
+    result.textContent="Play Game"
 option.addEventListener("click",(e)=>{
-    console.log(user.src);
-    user.src=e.target.src;
-    result.textContent="Wait....";
+    let userSelectedImage=e.target.src;
+    user.src=userSelectedImage;
+    result.textContent="Wait...";
+
+    const cpuResponse=Math.floor(Math.random()*3);
+    cpu.src=values[cpuResponse];
+
+    let cpuValue=['R','P','S'][cpuResponse];
+    let userValue=['R','P','S'][e.target.alt];
+
+    let outcomes={
+        RR:"Draw",
+        RP:"Cpu",
+        RS:"User",
+        PP:"Draw",
+        PR:"User",
+        PS:"Cpu",
+        SS:"Draw",
+        SR:"CPU",
+        SP:"User",
+    }
+
+    let outcomeValue= outcomes[userValue+cpuValue];
+    result.textContent=userValue===cpuValue?"Match Draw":`${outcomeValue} Won!!`
 });
 }
 
